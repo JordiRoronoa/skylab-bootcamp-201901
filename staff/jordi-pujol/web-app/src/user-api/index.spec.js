f6e509d67e95f064a27306bdaa19b6dff464383a
Jordi <jordi.pujolg@gmail.com>
2019-02-14 09:28:36 +0100

    plus userapi testing

--- staff/jordi-pujol/web-app/src/user-api/index.spec.js
+++ staff/jordi-pujol/web-app/src/user-api/index.spec.js
@@ -3,7 +3,9 @@
 const { expect } = require('chai')
 const userApi = require('.')
 
+
 describe('user api', () => {
+
     describe('register', () => {
         const name = 'Manuel'
         const surname = 'Barzi'
@@ -26,7 +28,23 @@ describe('user api', () => {
                 })
         )
 
-        // TODO more unit test cases
+        it('should fail on empty name', ()=> {
+            expect( () => userApi.register('', surname, username, password)).to.throw(Error, 'name is empty')
+        })
+
+        it('should fail on empty surname', ()=> {
+            expect( () => userApi.register(name, '', username, password)).to.throw(Error, 'surname is empty')
+        })
+        it('should fail on empty username', ()=> {
+            expect( () => userApi.register(name, surname, '', password)).to.throw(Error, 'username is empty')
+        })
+        it('should fail on empty password', ()=> {
+            expect( () => userApi.register(name, surname, username, '')).to.throw(Error, 'password is empty')
+        })
+        it('should fail when name is not a string', () => {
+            const name = true
+            expect( () => userApi.register(name, surname, username, password)).to.throw(Error, `${name} is not a string`)
+        })
     })
 
     describe('authenticate', () => {
@@ -37,10 +55,13 @@ describe('user api', () => {
 
         let _id
 
-        beforeEach(() =>
+        beforeEach( () =>
             userApi.register(name, surname, username, password)
                 .then(id => _id = id)
-        )
+                .catch(error => {
+                    expect(error.message).to.equal(`user with username \"${username}\" already exists`)
+                }
+        ))
 
         it('should succeed on correct data', () =>
             userApi.authenticate(username, password)
@@ -49,8 +70,33 @@ describe('user api', () => {
                     expect(token).to.exist
                 })
         )
+        it ('should fail if user is not registered',() =>
+            userApi.authenticate('potato', password)
+                .then ( () => {
+                    throw Error ('should not have passed by here')    
+                })
+                .catch(error => {
+                    expect(error).to.exist
+                    expect(error.message).to.equal(`user with username \"${'potato'}\" does not exist`)
+                })
+        )
+        it ('should fail if password is incorrect',() =>
+            userApi.authenticate(username, '456')
+                .then ( ()=> {
+                    throw Error ('should not have passed by here')    
+                })
+                .catch(error => {
+                    expect(error).to.exist
+                    expect(error.message).to.equal(`username and/or password wrong`)
+                })
+        )
+        it('should fail on empty username', ()=> {
+            expect( () => userApi.authenticate('', password)).to.throw(Error, 'username is empty')
+        })
+        it('should fail on empty password', ()=> {
+            expect( () => userApi.authenticate(username, '')).to.throw(Error, 'password is empty')
+        })
 
-        // TODO more unit test cases
     })
 
     describe('retrieve', () => {
@@ -66,6 +112,9 @@ describe('user api', () => {
                 .then(id => _id = id)
                 .then(() => userApi.authenticate(username, password))
                 .then(({ token }) => _token = token)
+                .catch (error =>{
+                    expect(error.message).to.equal(`user with username \"${username}\" already exists`)
+                })
         )
 
         it('should succeed on correct data', () =>
@@ -77,8 +126,32 @@ describe('user api', () => {
                     expect(user.username).to.equal(username)
                 })
         )
-
-        // TODO more unit test cases
+        it ('should fail on incorrect id',() =>
+            userApi.retrieve('potato', _token)
+                .then ( () => {
+                    throw Error ('should not have passed by here')    
+                })
+                .catch(error => {
+                    expect(error).to.exist
+                    expect(error.message).to.equal(`token id \"${_id}\" does not match user \"${'potato'}\"`)
+                })        
+        )
+        it ('should fail on incorrect token',() =>
+        userApi.retrieve(_id, 'potato')
+            .then ( () => {
+                throw Error ('should not have passed by here')    
+            })
+            .catch(error => {
+                expect(error).to.exist
+                expect(error.message).to.equal('invalid token')
+            })        
+        )
+        it('should fail on empty id', ()=> {
+            expect( () => userApi.retrieve('', _token)).to.throw(Error, 'id is empty')
+        })
+        it('should fail on empty token', ()=> {
+            expect( () => userApi.retrieve(_id , '')).to.throw(Error, 'token is empty')
+        })
     })
 
     describe('update', () => {
@@ -94,6 +167,9 @@ describe('user api', () => {
                 .then(id => _id = id)
                 .then(() => userApi.authenticate(username, password))
                 .then(({ token }) => _token = token)
+                .catch (error => {
+                    expect(error.message).to.equal(`user with username \"${username}\" already exists`)
+                })
         )
 
         it('should succeed on correct data', () => {
@@ -110,7 +186,39 @@ describe('user api', () => {
                 })
         })
 
-        // TODO more unit test cases
+        it('should fail on incorrect id', ()=> {
+        const data = { name: 'Pepito', surname: 'Grillo', age: 32 }
+
+            return userApi.update('potato', _token, data)       
+                .then(() => {
+                    throw Error ('should not have passed by here')    
+                })
+                .catch(error => {
+                    expect(error).to.exist
+                    expect(error.message).to.equal(`token id \"${_id}\" does not match user \"${'potato'}\"`)
+            })  
+        })
+        it('should fail on incorrect token', ()=> {
+            const data = { name: 'Pepito', surname: 'Grillo', age: 32 }
+
+                return userApi.update(_id, 'potato', data)
+                    .then(()=> { 
+                        throw Error ('should not have passed by here')
+                    })
+                    .catch(error => {
+                        expect(error).to.exist
+                        expect(error.message).to.equal('invalid token')
+                    })
+        })
+        it('should fail on empty id', () => {
+            const data = { name: 'Pepito', surname: 'Grillo', age: 32 }
+            expect(() => userApi.update('', _token, data)).to.throw(Error, 'id is empty')
+        })
+        it('should fail on empty token', () => {
+            const data = { name: 'Pepito', surname: 'Grillo', age: 32 }
+            expect( () => userApi.update(_id, '', data)).to.throw(Error, 'token is empty')
+        })
+        
     })
 
     describe('remove', () => {
@@ -126,6 +234,9 @@ describe('user api', () => {
                 .then(id => _id = id)
                 .then(() => userApi.authenticate(username, password))
                 .then(({ token }) => _token = token)
+                .catch(error =>{
+                    expect(error.message).to.equal(`user with username \"${username}\" already exists`)
+                })
         )
 
         it('should succeed on correct data', () => {
@@ -134,9 +245,71 @@ describe('user api', () => {
                 .then(() => {
                     throw Error('should not pass by here')
                 })
-                .catch(({message}) => expect(message).to.equal(`user with id \"${_id}\" does not exist`))
+                .catch(({message}) => expect(message).to.equal(`user with id "${_id}" does not exist`))
         })
-
-        // TODO more unit test cases
+        it('should fail when id is incorrect', () => {
+            userApi.remove('potato', _token, username, password)
+                .then(() => {
+                    throw Error ('should not have passed by here')
+                })
+                .catch(error => {
+                    expect(error).to.exist
+                    expect(error.message).to.equal(`token id \"${_id}\" does not match user \"${'potato'}\"`)
+                })
+        })
+        it('should fail when token is incorrect', ()=>{
+            userApi.remove(_id, 'potato', username, password)
+                .then(()=> {
+                    throw Error ('should not have passed by here')
+                })
+                .catch(error => {
+                    expect(error).to.exist
+                    expect(error.message).to.equal('invalid token')
+                })
+        })
+        it('should fail when username does not match', () =>
+        userApi.remove(_id, _token, 'potato', password)
+            .then( () => {
+                throw Error ('should not have passed by here')})
+            .catch(error => {
+                expect(error).to.exist
+                expect(error.message).to.equal('username and/or password wrong')
+            })
+        )
+        it('should fail when password does not match', () =>
+            userApi.remove(_id, _token, username, '444')
+                .then( () => {
+                    throw Error ('should not have passed by here')
+                })
+                .catch (error => {
+                    expect(error).to.exist
+                    expect(error.message).to.equal('username and/or password wrong')
+                })
+        )
+        it('should fail on empty id', () =>{
+            expect( ()=> userApi.remove('', _token, username, password)).to.throw(Error, 'id is empty')
+        })
+        it('should fail on empty token', ()=> {
+            expect( () => userApi.remove(_id, '', username, password)).to.throw(Error, 'token is empty')
+        })
+        it('should fail on undefined token', ()=> {
+            const _token = undefined
+            expect( () => userApi.remove(_id, _token, username, password)).to.throw(Error, `${_token} is not a string`)
+        })
+        it('should fail on empty username', () => {
+            expect( () => userApi.remove(_id, _token, '', password)).to.throw(Error, 'username is empty')
+        })
+        it('should fail on undefined username', ()=> {
+            const username = undefined
+            expect( () => userApi.remove(_id, _token, username, password)).to.throw(Error, `${username} is not a string`)
+        })
+        it('should fail on empty password', () => {
+            const password = ''
+            expect( () => userApi.remove(_id, _token, username, password)).to.throw(Error, 'password is empty')    
+        })
+        it('should fail on undefined password', ()=> {
+            const password = undefined
+            expect( () => userApi.remove(_id, _token, username, password)).to.throw(Error, `${password} is not a string`)
+        })
     })
 })
